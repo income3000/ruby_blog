7cabedbd21842dd21f19d0f68100f784a403a22a
income3000 <income3000@gmail.com>
2021-10-31 01:50:14 -0400

    third  commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
-class Comment < ApplicationRecord
-    belongs_to :article 
+class Article < ApplicationRecord
+    has_many :comments
+  
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles
 
-end
+  resources :articles do 
+    resources :comments
+  end
+end 
